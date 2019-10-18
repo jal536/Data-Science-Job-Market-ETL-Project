d66082c334ca7974ad367d425a576a69c4ba9e63
jiebai999 <jiebai999@gmail.com>
2019-10-17 19:09:21 -0500

    version with analysis uploaded

---
+++ ETL.sql
@@ -0,0 +1,16 @@
+CREATE TABLE datascience_location (
+   position VARCHAR,
+   company VARCHAR,
+   location VARCHAR
+);
+
+CREATE TABLE datascience_skill (
+   job_type VARCHAR,
+   skill VARCHAR,
+   company VARCHAR
+);
+
+CREATE TABLE location_distribution (
+   state VARCHAR,
+   percentage DEC
+)
